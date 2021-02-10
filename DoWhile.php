caa938ff0fe5207e329ff2152d869062cc4cd413
NikitaBalasahebRaut <68865567+NikitaBalasahebRaut@users.noreply.github.com>
2021-02-10 19:27:40 +0530

    Add files via upload

---
+++ DoWhile.php
@@ -0,0 +1,13 @@
+<?php
+
+     $i = 0;
+	 do
+	 {
+	   echo $i;
+	   $i++;
+	 }
+	 while($i < 10);
+	 
+	 echo"Loop stopped at i = $i";
+	 
+?>
---
+++ SwitchCase.php
@@ -0,0 +1,15 @@
+<?php
+      $name = "nikita";
+	  
+	  switch($name)
+	  {
+        case "nikita":
+		echo "my name is nikita";
+		break;
+		case "suchita":
+		echo "my name is suchita";
+		break;
+		default:
+		echo "sorry";
+      }
+?>
